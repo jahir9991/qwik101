6d623da0d763a28e0496fe8340e848d89906f999
jahir alam raju <work.jahiralam@gmail.com>
2022-03-26 13:47:42 +0600

    yy

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import { component$, $ } from "@builder.io/qwik";
+
+export const Home = component$(() => {
+  return $(() => {
+    return (
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+        impedit vitae quia accusamus temporibus facere esse et? Alias ullam
+        atque, tempora natus amet magnam. Rerum expedita veritatis enim quia
+        dolorem?
+      </div>
+    );
+  });
+});
